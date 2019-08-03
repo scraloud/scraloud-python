fb1decfec52d2baf314717028c04f4609d528fbd
Musab Gültekin <gultekinmusab@gmail.com>
2019-08-03 15:32:23 +0300

    Used params parameter on requests lib

--- scraloud/client.py
+++ scraloud/client.py
@@ -6,6 +6,6 @@ import requests
 def export(data):
     item_url = os.environ.get("SCRALOUD_ITEM_URL")
     spider_id = os.environ.get("SCRALOUD_SPIDER_ID")
-    url = item_url + "?spider_id=" + spider_id
-    response = requests.post(url, json=data)
+    params = {"spider_id": spider_id}
+    response = requests.post(item_url, params=params, json=data)
     return response
