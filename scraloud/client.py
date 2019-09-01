194087b489a352547077fe052558fac86eda44c7
Musab Gültekin <gultekinmusab@gmail.com>
2019-09-01 16:47:39 +0300

    Spider changed to scraper

--- scraloud/client.py
+++ scraloud/client.py
@@ -5,7 +5,7 @@ import requests
 
 def export(data):
     item_url = os.environ.get("SCRALOUD_ITEM_URL")
-    spider_id = os.environ.get("SCRALOUD_SPIDER_ID")
-    params = {"spider_id": spider_id}
+    scraper_id = os.environ.get("SCRALOUD_SCRAPER_ID")
+    params = {"scraper_id": scraper_id}
     response = requests.post(item_url, params=params, json=data)
     return response
--- tests/test_export.py
+++ tests/test_export.py
@@ -4,9 +4,9 @@ from scraloud import export
 
 def test_export_data():
     os.environ.setdefault("SCRALOUD_ITEM_URL", "https://httpbin.org/post")
-    os.environ.setdefault("SCRALOUD_SPIDER_ID", "1")
+    os.environ.setdefault("SCRALOUD_SCRAPER_ID", "1")
     data = {"key":"value"}
     resp = export(data)
     assert resp.status_code == 200
     assert resp.json()["data"] == '{"key": "value"}'
-    assert resp.json()["args"]["spider_id"] == "1"
+    assert resp.json()["args"]["scraper_id"] == "1"
